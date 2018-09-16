2cab5de83dea494ab5157072e1e9acf5a820f2d2
devwiltbank <devin.wiltbank@gmail.com>
2018-09-15 21:12:03 -0700

    updated visible message

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code here
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
+var http = require("http");
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World.  This is our HTTP server running on localhost:8080");
+  })
+  .listen(8080, "127.0.0.1");
 
-})
-.listen(8080, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:8080/');
-console.log('myMessage');
+console.log("Server running at http://127.0.0.1:8080/");
+console.log("myMessage");
