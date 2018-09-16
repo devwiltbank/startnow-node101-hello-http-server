899302461f64edeeea9bd4564b4695e34517af21
devwiltbank <devin.wiltbank@gmail.com>
2018-09-15 21:17:51 -0700

    New console.log message

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // write your code here
 var http = require("http");
+var myMessage = "This is the console.log message";
 http
   .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.writeHead(200, { "Content-Type": "text/css" });
     res.end("Hello World.  This is our HTTP server running on localhost:8080");
   })
   .listen(8080, "127.0.0.1");
 
 console.log("Server running at http://127.0.0.1:8080/");
-console.log("myMessage");
+console.log(myMessage);
